8df0de4050ba5ea1ebd4de46e57aa96ced1746a8
Pohrib Rebecca <PohribRebecca@users.noreply.github.com>
2020-05-12 18:46:34 +0300

    API request handling requested

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MainComponent} from './main';
+import {DespreComponent} from './despre';
+import {ContactComponent} from './contact';
+import {MedicamenteComponent} from './medicamente';
+import {RecomandaremedComponent} from './recomandaremed';
+import {RatingComponent} from './rating';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'main' , component : MainComponent},
+  {path: 'despre' , component: DespreComponent},
+  {path: 'contact' , component: ContactComponent},
+  {path: 'medicamente' , component: MedicamenteComponent},
+  {path: 'recomandaremed' , component: RecomandaremedComponent },
+  {path: 'rating' , component: RatingComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routesConstants = [MainComponent , DespreComponent, ContactComponent, MedicamenteComponent, RecomandaremedComponent, RatingComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,161 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
+import { FooterComponent } from './footer/footer.component';
+import { MiddlePicturesComponent } from './middle-pictures/middle-pictures.component';
+import { BoxesComponent } from './boxes/boxes.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import { NgImageSliderModule } from 'ng-image-slider';
+import {DespreComponent} from './despre';
+import {MainComponent} from './main';
+import {MatTabsModule} from '@angular/material/tabs';
+import {ContactComponent} from './contact/contact.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule} from '@angular/material/form-field';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatCardModule} from '@angular/material/card';
+import {PortalModule} from '@angular/cdk/portal';
+import {ScrollingModule} from '@angular/cdk/scrolling';
+import {ClipboardModule} from '@angular/cdk/clipboard';
+import {CdkStepperModule} from '@angular/cdk/stepper';
+import {CdkTableModule} from '@angular/cdk/table';
+import {CdkTreeModule} from '@angular/cdk/tree';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatInputModule} from '@angular/material/input';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatNativeDateModule, MatRippleModule} from '@angular/material/core';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatSortModule} from '@angular/material/sort';
+import {MatTableModule} from '@angular/material/table';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatTreeModule} from '@angular/material/tree';
+import {MedicamenteComponent} from './medicamente';
+import {RecomandaremedComponent} from './recomandaremed/recomandaremed.component';
+import {RatingComponent} from './rating';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MainNavComponent } from './main-nav/main-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+
+    AppComponent,
+    FooterComponent,
+    MiddlePicturesComponent,
+    BoxesComponent,
+    MenuBarComponent,
+    DespreComponent,
+    MainComponent,
+    ContactComponent,
+    MedicamenteComponent,
+    RecomandaremedComponent,
+    RatingComponent,
+    MainNavComponent
   ],
   imports: [
+
+    NgbModule,
     BrowserModule,
-    AppRoutingModule
+    MatTabsModule,
+    NgImageSliderModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatIconModule,
+    MatButtonModule,
+    MatChipsModule,
+    BrowserModule,
+    MatSliderModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatRadioModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatCardModule,
+    MatToolbarModule,
+    ReactiveFormsModule,
+    PortalModule,
+    ScrollingModule,
+
+    ClipboardModule,
+    CdkStepperModule,
+    CdkTableModule,
+    CdkTreeModule,
+    DragDropModule,
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatBottomSheetModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+
+    MatRippleModule,
+
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+
+    MatTooltipModule,
+    MatTreeModule,
+    FlexLayoutModule,
+    NgbModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' }}],
+  bootstrap: [AppComponent,
+    MedicamenteComponent, ContactComponent, RatingComponent ],
+  entryComponents: [MedicamenteComponent, ContactComponent, RatingComponent ]
 })
 export class AppModule { }
